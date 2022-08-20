05eb0e1bd3a7f298ceca2be47f1c802b16c1adff
Guilherme <gui-clemente@hotmail.com>
2022-08-20 04:15:08 -0300

    Initial commit

---
+++ CodeFile1.cs
@@ -0,0 +1,65 @@
+﻿namespace ex1
+{
+    class jogo
+    {
+        private int id;
+        private DateTime lancamento;
+        private string nome;
+        private string categoria;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("id tem que ser maior que zero");
+                else { id = value; }
+
+            }
+        }
+        public DateTime Lancamento
+        {
+            get => lancamento;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new Exception("jogo nao lançado");
+                }
+
+                else { lancamento = value; }
+            }
+        }
+        public string Nome
+        {
+            get => nome;
+            set
+            {
+                if (value.Length < 2)
+                    throw new Exception("o nome precisa ter no minino 2 caracteres");
+                else { nome = value; }
+            }
+        }
+
+
+
+        public string Categoria
+        {
+            get => categoria;
+            set
+            {
+                if (value != "ação" || value != "Esporte" || value != "luta" || value != "tiro")
+                {
+                    throw new Exception("válido apenas categoria de “ação”  “luta” “tiro” e “Esporte”");
+                }
+                else { categoria = value; }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nId do carro " + id + "\n nome " + nome + "\ncategoria " + categoria + "\ndata de lançamento" + lancamento;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ex1;
+
+jogo jogo = new jogo();
+
+
+do
+{
+    try
+    {
+
+
+        Console.WriteLine("id do jogo");
+        jogo.Id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("nome do jogo");
+        jogo.Nome = Console.ReadLine();
+
+        Console.WriteLine("data de lancamento");
+        jogo.Lancamento = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("categoria");
+        jogo.Categoria = Console.ReadLine();
+
+
+
+    }
+    catch (Exception erro)
+    {
+        Console.WriteLine(erro.Message);
+    }
+} while (true);
+
+
+
+Console.WriteLine(jogo.ToString());
+
+
